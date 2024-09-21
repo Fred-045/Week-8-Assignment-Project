0175127fd229c357df7e876dcf69b5d6f2457c23
Fred-045 <mwangifrederick773@gmail.com>
2024-09-21 20:25:00 +0300

    Add files via upload

---
+++ facilies 2.sql
@@ -0,0 +1,60 @@
+/*
+-- Query: SELECT * FROM nairobi_hospital.facilities
+LIMIT 0, 1000
+
+-- Date: 2024-09-21 20:19
+*/
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5421,'Kenyatta National Hospital','Hospital','WHO',2500,700,30,'Psychiatrist',250);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5422,'Mbagathi Hospital','Clinic','World Bank',1800,540,20,'Pediatrician',220);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5423,'Aga Khan University Hospital','Referral','KNH',3200,750,40,'Neurologist',290);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5424,'Nairobi Hospital','Hospital','UNICEF',2100,660,25,'Emergency med',270);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5425,'Moi Teaching and Referral Hospital','Clinic','WHO',1700,520,18,'Cardiologist',230);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5426,'Mater Misericordiae Hospital','Referral','World Bank',3000,700,35,'Psychiatrist',260);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5427,'Pumwani Maternity Hospital','Hospital','KNH',2400,680,30,'Pediatrician',280);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5428,'Karen Hospital','Clinic','UNICEF',1400,510,15,'Neurologist',200);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5429,'Coptic Hospital','Referral','WHO',2800,730,45,'Emergency med',290);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5430,'St. Mary\'s Mission Hospital','Hospital','World Bank',3600,780,50,'Cardiologist',300);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5431,'Jaramogi Oginga Odinga Teaching & Referral','Clinic','KNH',1600,530,20,'Psychiatrist',220);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5432,'Kijabe Hospital','Referral','UNICEF',4000,790,42,'Pediatrician',250);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5433,'Tenwek Mission Hospital','Hospital','WHO',2100,620,30,'Neurologist',240);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5434,'Gertrude\'s Children\'s Hospital','Clinic','World Bank',1700,550,18,'Emergency med',200);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5435,'Jubilee Health Centre','Referral','KNH',3200,700,35,'Cardiologist',275);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5436,'Mediheal Group of Hospitals','Hospital','UNICEF',2600,680,40,'Psychiatrist',230);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5437,'Avenue Hospital','Clinic','WHO',1900,540,23,'Pediatrician',210);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5438,'MP Shah Hospital','Referral','World Bank',3500,760,48,'Neurologist',290);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5439,'The Nairobi Women\'s Hospital','Hospital','KNH',2200,670,28,'Emergency med',250);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5440,'St. Francis Community Hospital','Clinic','UNICEF',1500,520,20,'Cardiologist',200);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5441,'Ruaraka Uhai Neema Hospital','Referral','WHO',2800,740,45,'Psychiatrist',270);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5442,'The Nairobi Spine and Orthopaedic Centre','Hospital','World Bank',3400,780,50,'Pediatrician',300);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5443,'Kakamega County Referral Hospital','Clinic','KNH',1600,530,18,'Neurologist',210);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5444,'Embu Level 5 Hospital','Referral','UNICEF',2900,710,36,'Emergency med',230);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5445,'Thika Level 5 Hospital','Hospital','WHO',2300,670,29,'Cardiologist',250);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5446,'Machakos Level 5 Hospital','Clinic','World Bank',1800,560,22,'Psychiatrist',240);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5447,'Meru Level 5 Hospital','Referral','KNH',3200,720,38,'Pediatrician',290);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5448,'Kisii Teaching and Referral Hospital','Hospital','UNICEF',2500,660,30,'Neurologist',270);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5449,'Nyeri County Referral Hospital','Clinic','WHO',2000,550,25,'Emergency med',220);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5450,'Bungoma County Referral Hospital','Referral','World Bank',3300,740,44,'Cardiologist',270);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5451,'Vihiga County Referral Hospital','Hospital','KNH',2600,690,32,'Psychiatrist',260);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5452,'Busia County Referral Hospital','Clinic','UNICEF',1600,530,20,'Pediatrician',220);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5453,'Homabay County Referral Hospital','Referral','WHO',2900,720,38,'Neurologist',280);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5454,'Siaya County Referral Hospital','Hospital','World Bank',3700,780,50,'Emergency med',300);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5455,'Bomet County Referral Hospital','Clinic','KNH',1500,510,18,'Cardiologist',200);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5456,'Kericho County Referral Hospital','Referral','UNICEF',3100,750,45,'Psychiatrist',290);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5457,'Kilifi County Referral Hospital','Hospital','WHO',2200,670,28,'Pediatrician',250);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5458,'Mombasa County Referral Hospital','Clinic','World Bank',1800,540,20,'Neurologist',230);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5459,'Kwale County Referral Hospital','Referral','KNH',3300,750,40,'Emergency med',290);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5460,'Tana River County Referral Hospital','Hospital','UNICEF',2700,700,35,'Cardiologist',270);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5461,'Lamu County Referral Hospital','Clinic','WHO',1400,520,15,'Psychiatrist',220);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5462,'Garissa County Referral Hospital','Referral','World Bank',3600,770,48,'Pediatrician',300);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5463,'Isiolo County Referral Hospital','Hospital','KNH',2500,650,32,'Neurologist',240);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5464,'Marsabit County Referral Hospital','Clinic','UNICEF',1300,510,12,'Emergency med',210);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5465,'Samburu County Referral Hospital','Referral','WHO',3100,730,40,'Cardiologist',260);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5466,'Laikipia County Referral Hospital','Hospital','World Bank',4000,790,50,'Psychiatrist',290);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5467,'Nakuru County Referral Hospital','Clinic','KNH',1700,530,18,'Pediatrician',220);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5468,'Narok County Referral Hospital','Referral','UNICEF',3000,710,45,'Neurologist',280);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5469,'Uasin Gishu County Referral Hospital','Hospital','WHO',2700,670,30,'Emergency med',250);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5470,'Elgeyo Marakwet County Referral Hospital','Clinic','World Bank',1600,540,22,'Cardiologist',220);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5471,'Nandi County Referral Hospital','Referral','KNH',3500,760,47,'Psychiatrist',300);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5472,'Trans Nzoia County Referral Hospital','Hospital','UNICEF',2300,680,28,'Pediatrician',260);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5473,'West Pokot County Referral Hospital','Clinic','WHO',1900,550,20,'Neurologist',240);
+INSERT INTO `` (`Facility_ID`,`Name`,`Type`,`Partnership`,`Capacity`,`Beds`,`Abulances`,`Doctors`,`Nurses`) VALUES (5474,'Turkana County Referral Hospital','Referral','World Bank',3100,740,42,'Emergency med',290);
